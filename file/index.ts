540ba23ac74b8a232a9bcfdab2c1e7e8b1d16f1c
서재원 <cinos81@gmail.com>
2020-12-10 15:14:26 +0900

    코멘트 형상 변경

--- file/index.ts
+++ file/index.ts
@@ -3,27 +3,37 @@ import session from 'express-session';
 import sessionFileStore from 'session-file-store';
 import cookieParser from 'cookie-parser'
 
-// https://dev.to/chris927/extending-express-types-with-typescript-declaration-merging-typescript-4-3jh
+/**
+ * 참조: https://dev.to/chris927/extending-express-types-with-typescript-declaration-merging-typescript-4-3jh
+ */
 declare module "express-session" {
 	interface Session {
 		num?: number;
 	}
 }
 
-// express 인스턴스 설정
+/**
+ * express 인스턴스 설정
+ */
 const app = express()
 
-// 쿠키 파서 미들웨어 초기화
+/**
+ * 쿠키 파서 미들웨어 초기화
+ */
 app.use(cookieParser())
 
-// 스토어 인스턴스 초기화
+/**
+ * 스토어 인스턴스 초기화
+ */
 const FileStore = sessionFileStore(session)
 const store = new FileStore()
 
 const oneSecond = 1000
 const oneMinute = oneSecond * 60
 
-// 세션 스토어 초기화
+/**
+ * 세션 스토어 초기화
+ */
 app.use(session({
 	secret: 'keyboard cat',
 	resave: false,
@@ -34,7 +44,9 @@ app.use(session({
 	}
 }))
 
-// 라우터 초기화
+/**
+ * 라우터 초기화
+ */
 app.get('/', (req, res) => {
 	console.log(req.sessionID, req.session);
 	const { num = 0 } = req.session;
@@ -43,5 +55,7 @@ app.get('/', (req, res) => {
 	return res.json(body)
 })
 
-// 서버 실행
+/**
+ * 서버 실행
+ */
 app.listen(3000, () => console.log('started at 3000 port'))
